6a1736c7167d12aed4270cb04bd97fd4780f5a0f
Jarvis <823867852@qq.com>
2022-05-07 19:32:31 +0800

    🙈 修改gitignore

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@
  * @Author: Jarvis 823867852@qq.com
  * @Date: 2022-05-06 16:58:48
  * @LastEditors: Jarvis 823867852@qq.com
- * @LastEditTime: 2022-05-06 19:55:14
+ * @LastEditTime: 2022-05-07 19:30:00
  * @FilePath: \beautiful-language\vite.config.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -28,5 +28,8 @@ export default defineConfig({
     alias: {
       '@': resolve(__dirname, 'src')
     }
+  },
+  build: {
+    outDir: 'doc'
   }
 })
