c94f55335616e5fc8a06bc6f465b3d9627135d00
Jarvis <823867852@qq.com>
2022-05-08 14:45:38 +0800

    🔧 还原outdir的配置

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@
  * @Author: Jarvis 823867852@qq.com
  * @Date: 2022-05-06 16:58:48
  * @LastEditors: Jarvis 823867852@qq.com
- * @LastEditTime: 2022-05-08 13:53:06
+ * @LastEditTime: 2022-05-08 14:43:36
  * @FilePath: \beautiful-language\vite.config.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -28,9 +28,6 @@ export default defineConfig({
     alias: {
       '@': resolve(__dirname, 'src')
     }
-  },
-  build: {
-    outDir: 'docs'
   },
   base: './'
 })
