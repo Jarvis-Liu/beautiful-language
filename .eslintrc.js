c1f8e1ce969f634c65faefa1cedbac60961a0404
Jarvis <823867852@qq.com>
2022-05-08 13:00:14 +0800

    🔧 修改eslint，避免未声明引入definePrps时报错

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,7 @@
  * @Author: Jarvis 823867852@qq.com
  * @Date: 2022-05-06 17:14:58
  * @LastEditors: Jarvis 823867852@qq.com
- * @LastEditTime: 2022-05-06 17:28:50
+ * @LastEditTime: 2022-05-08 12:56:56
  * @FilePath: \beautiful-language\.eslintrc.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -23,5 +23,9 @@ module.exports = {
     'vue'
   ],
   rules: {
+  },
+  globals: {
+    defineProps: 'readonly',
+    defineEmits: 'readonly'
   }
 }
--- src/views/highQuality/components/nameConfirm.vue
+++ src/views/highQuality/components/nameConfirm.vue
@@ -2,7 +2,7 @@
  * @Author: Jarvis 823867852@qq.com
  * @Date: 2022-05-07 14:33:06
  * @LastEditors: Jarvis 823867852@qq.com
- * @LastEditTime: 2022-05-07 17:31:03
+ * @LastEditTime: 2022-05-08 12:57:01
  * @FilePath: \beautiful-language\src\views\highQuality\components\nameConfirm.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -31,7 +31,6 @@
 </template>
 
 <script setup>
-import { defineProps, defineEmits } from 'vue'
 defineProps({
   name: {
     type: String,
