58d10da3e1fa9be6bef1eb1cfc3affd0890cb66b
Jarvis <823867852@qq.com>
2022-05-08 22:10:04 +0800

    🐛 修改不正确的引用

--- src/router/index.js
+++ src/router/index.js
@@ -2,17 +2,18 @@
  * @Author: Jarvis 823867852@qq.com
  * @Date: 2022-05-06 21:33:38
  * @LastEditors: Jarvis 823867852@qq.com
- * @LastEditTime: 2022-05-08 21:56:54
+ * @LastEditTime: 2022-05-08 22:09:45
  * @FilePath: \beautiful-language\src\router\index.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 import { createRouter, createWebHashHistory } from 'vue-router'
+import Home from '@/views/layouts/home.vue'
 
 export const constantRouterMap = [
   {
     path: '/',
     name: 'home',
-    component: () => import('@/views/layouts/Home.vue'),
+    component: Home,
     meta: {
       title: 'beautiful-language'
     },
