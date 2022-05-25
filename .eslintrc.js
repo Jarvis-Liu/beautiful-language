8048b2ba23977b56e10fc94bf839dc81a13b77b2
Jarvis <823867852@qq.com>
2022-05-25 17:48:19 +0800

    🔧 更新eslint配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,23 +2,34 @@
  * @Author: Jarvis 823867852@qq.com
  * @Date: 2022-05-06 17:14:58
  * @LastEditors: Jarvis 823867852@qq.com
- * @LastEditTime: 2022-05-09 22:55:11
+ * @LastEditTime: 2022-05-25 17:47:23
  * @FilePath: \beautiful-language\.eslintrc.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 module.exports = {
   env: {
     browser: true,
-    es2021: true
+    es2021: true,
+    node: true
   },
-  extends: ['plugin:vue/vue3-recommended', 'prettier', '@vue/prettier'],
+  extends: [
+    'plugin:vue/vue3-recommended',
+    'eslint:recommended',
+    'prettier',
+    '@vue/prettier'
+  ],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module'
   },
   plugins: ['vue', 'prettier'],
   rules: {
-    endOfLine: 'auto'
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto'
+      }
+    ]
   },
   globals: {
     defineProps: 'readonly',
